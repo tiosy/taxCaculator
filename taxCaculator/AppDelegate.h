9184f8b9e7bbeefed6ce305260b6c38ec9df95ab
Timothy Yeh <timothy.yeh@gmail.com>
2015-02-27 16:42:13 -0800

    Initial Commit

---
+++ taxCaculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taxCaculator
+//
+//  Created by tim on 2/27/15.
+//  Copyright (c) 2015 Timothy Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
